ee22c096b23ee525be174bfec83b61e06ba3a25b
Thomas Mediavilla <thomas.mediav@gmail.com>
2020-06-17 14:04:49 +0200

    Scroll down arrow on landpage is now working

--- src/components/HomeDetails.vue
+++ src/components/HomeDetails.vue
@@ -8,7 +8,7 @@
                         >About us</v-flex
                     >
                     <v-flex>
-                        <div class="headline mt-3 white--text font-weight-bold">
+                        <div class="headline mt-3 white--text font-weight-bold" id="down">
                             Lorem ipsum
                         </div>
                         <p class="subheading mt-3">
