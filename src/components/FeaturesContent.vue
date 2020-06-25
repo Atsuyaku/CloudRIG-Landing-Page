d7ffccfa589debd9cbc46515bc0d10afac05dbf0
Thomas Mediavilla <thomas.mediav@gmail.com>
2020-06-25 10:06:15 +0200

    fixing eslint

--- src/components/FeaturesContent.vue
+++ src/components/FeaturesContent.vue
@@ -4,7 +4,7 @@
                    <v-card dark color="rgb(52, 73, 94)" class="pa-4" flat>
                                 <v-layout column>
                                <v-flex
-                                       class="display-2 font-weight-white white--text text-xs-center my-5"
+                                               class="display-2 font-weight-white white--text text-xs-center my-5"
                            >Features</v-flex
                     >
                     <v-flex>
