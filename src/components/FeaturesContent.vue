11ada34c950e64008f3e45444067f0c2e6492e78
Thomas Mediavilla <thomas.mediav@gmail.com>
2020-06-25 15:08:54 +0200

    Trying to get back eslint script

--- src/components/FeaturesContent.vue
+++ src/components/FeaturesContent.vue
@@ -5,16 +5,16 @@
                 <v-layout column>
                     <v-flex
                         class="display-2 font-weight-white white--text text-xs-center my-5"
-                         >Features</v-flex
+                        >Features</v-flex
                     >
-                         <v-flex>
+                    <v-flex>
                         <p class="text-h5 mt-3">
                             Stream games and applications for approx. $0.13/hr
                             <br /><br />
-                                   Schedule shut down for the end of the current
-                                billing hour (AWS charges by the hour)<br /><br />
+                            Schedule shut down for the end of the current
+                            billing hour (AWS charges by the hour)<br /><br />
                             2 minute termination warning overlay<br /><br />
-                                  Auto-saves your rig when you stop or are booted
+                            Auto-saves your rig when you stop or are booted
                             off<br /><br />
                             Attach, extend a separate game drive<br /><br />
                             Choose either g2.2xlarge and g3.4xlarge instance
